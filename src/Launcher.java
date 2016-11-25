89febee4e5b81512b578261208d2fcfe228e3808
Antoine Croisille <Antoine Croisille@10.188.119.30>
2016-11-25 09:37:09 +0100

    Premier commit test

---
+++ src/Launcher.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Antoine Croisille
+ *
+ */
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pandocreon Divinae, Hello World");
+	}
+
+}
